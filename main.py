8808a5bfbb5120af8d0b07d11f22707f83ebf8be
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2023-12-05 17:05:53 +0000

    Importing libs and main from FeatherS2
    Main libraries and main.py files imported from original source code on feather.

---
+++ main.py
@@ -0,0 +1,71 @@
+import time
+import board
+import busio
+import struct
+import adafruit_mlx90640
+
+
+FILE_PATH = "images/image.ppm"
+FILE_PATH_ASCII = "images/image.txt"
+
+i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
+
+mlx = adafruit_mlx90640.MLX90640(i2c)
+print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
+
+
+def float_list_to_bytearray(float_list):
+    byte_array = bytearray()
+
+    for value in float_list:
+        # Convert float to bytes using struct.pack
+        byte_representation = struct.pack('!f', value)
+
+        # Iterate over bytes and convert each to an integer
+        for byte in byte_representation:
+            byte_array.append(byte)
+
+    return byte_array
+
+# # Example usage:
+# float_list = [3.14, 2.718, 1.618]
+# # result_bytearray = float_list_to_bytearray(float_list)
+
+# print(result_bytearray)
+
+# if using higher refresh rates yields a 'too many retries' exception,
+# try decreasing this value to work with certain pi/camera combinations
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
+maxval = 255
+w = 0
+h = 0
+count = 1
+frame = [0] * 768
+t = None
+while count > 0:
+    try:
+        mlx.getFrame(frame)
+    except ValueError:
+        # these happen, no biggie - retry
+        continue
+
+    for h in range(24):
+        for w in range(32):
+            t = frame[h*32 + w]
+            # print("%0.1f, " % t, end="")
+        print()
+    print()
+    # for i in frame:
+    #     if i < 24:
+    #         print(i)
+    image = float_list_to_bytearray(frame)
+    # print((image))
+    # print(type(t))
+    count -= 1
+
+    ppm_header = f"P6 {w+1} {h+1} {maxval}\n"
+    print(ppm_header)
+    with open(FILE_PATH, "wb") as image_file:
+        #PPM Header - Portable PixMap
+        image_file.write(bytearray(ppm_header, 'ascii'))
+        image_file.write(image)
