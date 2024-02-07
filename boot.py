85f95d97c333d55de5fc20cfccd562d9a12624e0
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2024-02-07 15:53:47 +0000

    Update boot.py
    Using remount and pin D5 to start recording.

--- boot.py
+++ boot.py
@@ -1,15 +1,22 @@
-# SPDX-FileCopyrightText: 2017 Limor Fried for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
+import board, digitalio, time, storage
+from adafruit_debouncer import Debouncer
 
-import board
-import digitalio
-import storage
 
-switch = digitalio.DigitalInOut(board.D0)
-switch.direction = digitalio.Direction.INPUT
-switch.pull = digitalio.Pull.UP
+button_record = digitalio.DigitalInOut(board.D5)
+button_record.switch_to_input(pull=digitalio.Pull.UP)   #switch_to_input will wait until it detects an input from the button
 
-# If the D0 is connected to ground with a wire
-# CircuitPython can write to the drive
-storage.remount("/", False)
+
+# Debouncer function
+# def debouncable(pin):
+#     switch_io = digitalio.DigitalInOut(pin)
+#     switch_io.direction = digitalio.Direction.INPUT
+#     switch_io.pull = digitalio.Pull.UP
+#     return switch_io
+
+# button_b = Debouncer(debouncable(board.D21))  #IO38
+
+#create flag variables
+
+
+#if switch pin is connected to ground CircuitPython can write to the drive
+storage.remount("/", not(button_record.value))
